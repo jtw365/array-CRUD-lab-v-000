57d059b9341660b4df6199f070cfdd264e5e02ca
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-17 23:55:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  Array.new.unshift
+  element = [1,2,3,4]
+  element.unshift(1)
 end
 
 def remove_element_from_end_of_array(array)
